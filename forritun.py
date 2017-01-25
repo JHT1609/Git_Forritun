bf094c6a4eee1bd7b9b9e0618cf98f86a8ef54cf
JHT1609 <johannesto245@nemi.tskoli.is>
2017-01-25 10:06:53 +0000

    verkefni 3

--- forritun.py
+++ forritun.py
@@ -11,3 +11,15 @@ print("Tölurnar margfaldaðar saman eru",tala1*tala2,"samtals")
 fnfn=input("Sláðu inn fornafn þitt ")
 enfn=input("Sláðu inn eftir nafn þitt ")
 print("Halló",fnfn,enfn)
+
+#Verkefni 3
+tlj1=0
+tlj2=0
+txt=input("Sláðu inn texta")
+for x in txt:
+    if x.isalpha() and x.isupper():
+        tlj1=tlj1+1
+    if x.isalpha() and x.islower():
+        tlj2=tlj2+1
+print(tlj1)
+print(tlj2)
