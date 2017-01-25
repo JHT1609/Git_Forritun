d8c17ff4765696ab03d388fecdfaf7d108fa3a7d
JHT1609 <johannesto245@nemi.tskoli.is>
2017-01-25 09:45:11 +0000

    verkefni 1

---
+++ forritun.py
@@ -0,0 +1,8 @@
+#johannes
+#1/25/2017
+
+#Verkefni 1
+tala1=int(input("Sláðu inn tölu "))
+tala2=int(input("Sláðu inn aðra tölu "))
+print("Tölurnar lagðar saman eru",tala1+tala2,"samtals")
+print("Tölurnar margfaldaðar saman eru",tala1*tala2,"samtals")
